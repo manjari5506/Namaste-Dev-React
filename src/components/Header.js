d3d683578fdeffb9be3e4c31b775e4c74dcc7aed
Manjari <raikwar.manjari@gmail.com>
2024-03-11 18:13:42 +0530

    routing start

--- src/components/Header.js
+++ src/components/Header.js
@@ -17,9 +17,9 @@ const Header = () => {
             </div>
             <div>
                 <ul>
-                    <li><Link>Home</Link></li>
-                    <li><Link>About Us</Link></li>
-                    <li><Link>Contact Us</Link></li>
+                    <li><Link to="/">Home</Link></li>
+                    <li><Link to="/about">About Us</Link></li>
+                    <li><Link to="/contact">Contact Us</Link></li>
                     <li>Cart</li>
                     <button className="login" onClick={() => {
                         btnNameReact === "Login" ? setBtnNameReact("Logout") : setBtnNameReact("Login");
