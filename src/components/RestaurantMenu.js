c081ceac4314df629eec71c9b7c016ce77d0c4fa
Manjari <raikwar.manjari@gmail.com>
2024-03-17 22:57:14 +0530

    using custom hook

--- src/components/RestaurantMenu.js
+++ src/components/RestaurantMenu.js
@@ -1,22 +1,24 @@
 import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import Shimmer from "./Shimmer";
+import useRestaurantMenu from "../utils/useRestaurantMenu"
 
 const RestaurantMenu = () => {
-    const [resInfo, setResInfo] = useState(null);
+    //const [resInfo, setResInfo] = useState(null);
     const { resId } = useParams();
+    const resInfo = useRestaurantMenu(resId);
 
-    useEffect(() => {
-        fetchMenu();
-    }, []);
+    // useEffect(() => {
+    //     fetchMenu();
+    // }, []);
 
-    const fetchMenu = async () => {
-        const data = await fetch("" + resId);
-        const json = await data.json();
+    // const fetchMenu = async () => {
+    //     const data = await fetch("" + resId);
+    //     const json = await data.json();
 
-        console.log(json);
-        setResInfo(json);
-    }
+    //     console.log(json);
+    //     setResInfo(json);
+    // }
     if (resInfo === null) { return <Shimmer /> }
 
     const { name, cuisines, costForTwoMessage } = resInfo; //destructure from original dataset
