ccf73e190dfd3033af4bc399e9452fed56d7179a
Manjari <raikwar.manjari@gmail.com>
2024-03-23 23:52:19 +0530

    nn

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -3,6 +3,9 @@ const Contact = () => {
         <div>
             <h1>Contact page</h1>
             <h2>Welcome to contact page</h2>
+            <h3 className="address">Address</h3>
+            <h3 className="number">Number</h3>
+            <h3 className="pin">Pin</h3>
         </div>
     )
 }
